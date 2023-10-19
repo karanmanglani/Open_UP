417768808a5e188d7b1dc1541392299585bd53e4
karanmanglani <bkn45for@gmail.com>
2023-10-19 21:52:39 +0530

    Created a Basic user schema in mongoDB

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please tell us your email"],
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+
+  photo: String,
+
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: 8,
+  },
+
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
