d98efb0c5747785a379d5f5bd533e1f81ed985c7
karanmanglani <bkn45for@gmail.com>
2023-10-20 13:47:54 +0530

    Signup using jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,15 @@
 const User = require("./../models/userModel");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res, next) => {
   const newUser = await User.create(req.body);
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "Success",
+    token,
     data: {
       user: newUser,
     },
