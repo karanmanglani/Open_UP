99daa1e9dfb19ed5a082c7195be329dcd36ee698
karanmanglani <bkn45for@gmail.com>
2023-10-21 21:35:43 +0530

    Security agains XSS and NoSQL query injection

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const userRouter = require("./routes/userRoutes");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const app = express();
 
@@ -26,6 +28,13 @@ app.use("/api", limiter);
 app.use(express.static(`${__dirname}/public`));
 
 app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
