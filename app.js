ee50ede46b35b9fe63686e1473d27ea656a22814
karanmanglani <bkn45for@gmail.com>
2023-10-21 21:26:24 +0530

    Implemented rate limiting for protectoin from DDOS

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const rateLimit = require("express-rate-limit");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+// 1.) Global Middlewares
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
